d01ba2cdfb95d95d886b005a2399a955c6cf136e
Angus Russell <angus@brainblocks.io>
2019-02-27 13:21:20 +1100

    Made notistack props pluggable

--- src/components/snackbar/Snackbar.js
+++ src/components/snackbar/Snackbar.js
@@ -9,6 +9,7 @@ type Props = {
   infoIcon: React.Node,
   warningIcon: React.Node,
   children: React.Node,
+  notistackProps: Object,
   /** Given by destyle. Do not pass this to the component as a prop. */
   styles: Object
 }
@@ -20,30 +21,28 @@ export const Snackbar = ({
   errorIcon,
   infoIcon,
   warningIcon,
+  notistackProps,
   ...rest
-}: Props) => (
-  <SnackbarProvider
-    maxSnack={3}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right'
-    }}
-    classes={{
+}: Props) => {
+  const defaultNotistackProps = {
+    maxSnack: 3,
+    anchorOrigin: { vertical: 'bottom', horizontal: 'right' },
+    classes: {
       root: styles.root,
       variantSuccess: styles.success,
       variantError: styles.error,
       variantWarning: styles.warning,
       variantInfo: styles.info
-    }}
-    iconVariant={{
+    },
+    iconVariant: {
       success: <div className={styles.iconSuccess}>{successIcon}</div>,
       error: <div className={styles.iconError}>{errorIcon}</div>,
       warning: <div className={styles.iconWarning}>{warningIcon}</div>,
       info: <div className={styles.iconInfo}>{infoIcon}</div>
-    }}
-  >
-    {children}
-  </SnackbarProvider>
-)
+    }
+  }
+  const snackbarProps = { ...defaultNotistackProps, ...notistackProps }
+  return <SnackbarProvider {...snackbarProps}>{children}</SnackbarProvider>
+}
 
 export default destyle(Snackbar, 'BB-Snackbar')
